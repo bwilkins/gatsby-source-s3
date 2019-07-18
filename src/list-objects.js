162944c187a558e561234d904666db3d1d054d55
Brett Wilkins <brett@brett.geek.nz>
2019-07-18 11:51:06 +1000

    Attempt #1 at trying to fetch _all_ objects in a bucket

--- src/list-objects.js
+++ src/list-objects.js
@@ -1,11 +1,36 @@
 import AWS from 'aws-sdk';
 import omit from 'lodash.omit';
 
-/*
- * This is currently limited to 1000 objects by S3
- * if needed, NextContinuationToken can probably be used to get all
- */
-export const listObjects = (bucket, config = {}, maxKeys = 1000) => {
+const getObjects = (s3, bucket, continuationToken = null) => {
+  const params = {
+    ...(typeof bucket === 'string'
+      ? { Bucket: bucket }
+      : omit(bucket, 'Filter')),
+  };
+
+  continuationToken && (params.ContinuationToken = continuationToken);
+
+  return s3.listObjectsV2(params).promise();
+};
+
+const getAllObjects = (s3, bucket) => {
+  let allContent = {};
+
+  const fetchMore = content => {
+    Array.prototype.push.apply(allContent, content.Contents);
+    if (content.isTruncated) {
+      return getObjects(s3, bucket, content.NextContinuationToken).then(
+        fetchMore
+      );
+    } else {
+      return allContent;
+    }
+  };
+
+  return getObjects(s3, bucket).then(fetchMore);
+};
+
+export const listObjects = (bucket, config = {}) => {
   AWS.config.update(config);
 
   const s3 = new AWS.S3();
@@ -14,21 +39,13 @@ export const listObjects = (bucket, config = {}, maxKeys = 1000) => {
 
   return Promise.all(
     buckets.map(bucket => {
-      return s3
-        .listObjectsV2({
-          MaxKeys: maxKeys,
-          ...(typeof bucket === 'string'
-            ? { Bucket: bucket }
-            : omit(bucket, 'Filter')),
-        })
-        .promise()
-        .then(content => {
-          if (bucket.Filter) {
-            content.Contents = (content.Contents || []).filter(bucket.Filter);
-          }
+      return getAllObjects(s3, bucket).then(content => {
+        if (bucket.Filter) {
+          content = (content || []).filter(bucket.Filter);
+        }
 
-          return content;
-        });
+        return content;
+      });
     })
   );
 };
